b62d808efcda90973fbbf069031414ab0ebe37b6
Matheus Heimrath <129696259+Heimrath@users.noreply.github.com>
2024-09-29 20:56:24 -0300

    Resolvendo primeiro problema

--- conct_dados.py
+++ conct_dados.py
@@ -1,1 +1,12 @@
+# receber dois dados do usuario e concatenar em uma unica String
+
+# Recebe dois dados do usuário
+primeira_entrada = input("Digite a primeira informação: ")
+segunda_entrada = input("Digite a segunda informação: ")
+
+# Concatena as duas entradas em uma única string
+resultado = primeira_entrada + " " + segunda_entrada
+
+# Exibe o resultado
+print("Resultado da concatenação:", resultado)
 
